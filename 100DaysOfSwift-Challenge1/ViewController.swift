71d08968e0b3cce638c46cf3cf57f47af99b7e72
João Gabriel Dourado Cervo <joaogabrieldouradocervo@gmail.com>
2021-01-27 17:08:06 -0300

    ADD listagem dos paises na tableView

--- 100DaysOfSwift-Challenge1/ViewController.swift
+++ 100DaysOfSwift-Challenge1/ViewController.swift
@@ -8,6 +8,8 @@
 import UIKit
 
 class ViewController: UITableViewController {
+    var countriesSet = Set<String>()
+    var countries = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,9 +20,27 @@ class ViewController: UITableViewController {
         
         for item in itens {
             if item.hasSuffix(".png") {
-                // faz algo
+                let index = item.firstIndex(of: "@")!
+                countriesSet.insert(String(item[..<index]))
             }
         }
+        
+        countries = Array(countriesSet)
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return countries.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Country", for: indexPath)
+        
+        let index = indexPath.row
+        
+        cell.textLabel?.text = countries[index]
+        cell.imageView?.image = UIImage(named: countries[index])
+        
+        return cell
     }
 
 
